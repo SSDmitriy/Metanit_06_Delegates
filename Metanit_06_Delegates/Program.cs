d55a9497313f52fa569b4bce6b5fad20df0060ce
SSDmitiry <XYZmail@yandex.ru>
2023-01-09 11:21:58 +0500

    Add project files.

---
+++ Metanit_06_Delegates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Metanit_06_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
